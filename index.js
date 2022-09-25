8b181cc2308e63301fa9e8738fe996e51cb58a75
Edward Stone <ed@autotelic.com>
2022-09-25 14:51:31 -0700

    typo importing papertrail reporter

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { Logger } from './lib/logger/index.js'
 import { consoleReporter } from './lib/reporters/consoleReporter.js'
 import { createSeqReporter } from './lib/reporters/seqReporter.js'
 import { createLoggableEventHandler } from './lib/loggableEventHandler/index.js'
-import { createPapertrailHttpReporter } from './lib/reporters/paperTrailHttpReporter.js'
+import { createPapertrailHttpReporter } from './lib/reporters/papertrailHttpReporter.js'
 
 export {
   Logger,
