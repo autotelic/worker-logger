f551094d02e713db4c4f0e09e2ed93f322d2cae0
Edward Stone <ed@autotelic.com>
2022-09-28 22:19:57 -0700

    syslog formatting for papertrail http

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ import { consoleReporter } from './lib/reporters/consoleReporter.js'
 import { createSeqReporter } from './lib/reporters/seqReporter.js'
 import { createLoggableEventHandler } from './lib/loggableEventHandler/index.js'
 import { createPapertrailHttpReporter } from './lib/reporters/papertrailHttpReporter.js'
-import { processMessage as convertToSyslog } from './lib/syslogConvertor/rfc5424.js'
 
 export {
   Logger,
   createSeqReporter,
   consoleReporter,
   createLoggableEventHandler,
-  createPapertrailHttpReporter,
-  convertToSyslog
+  createPapertrailHttpReporter
 }
